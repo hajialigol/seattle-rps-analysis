59b4c28f30f92c7544a29b255816edcd2f481375
Daniel <danielhajialigol@gmail.com>
2020-07-10 16:40:04 -0400

    Added Spark functionality

--- Run/seattle_rps_run.R
+++ Run/seattle_rps_run.R
@@ -2,25 +2,36 @@
 # Remove all variables
 rm(list=ls())
 
+library(sparklyr)
+
+
 # Preset Variables for run script
-latest_date = "YYYY-mm-dd" # Enter date here
+latest_date = "" # Enter date here
 save_wd = ""
 data_wd = ""
+connection_type = ""
 
 # Load all source script functions
-source("~/GitHub/seattle-rps-analysis/Functions/FinancialProcessing/inflation_stock_source.R")
-source("~/GitHub/seattle-rps-analysis/Functions/Geocoding/zipcode_geocode_source.R")
-source("~/GitHub/seattle-rps-analysis/Functions/RPSProcessing/seattle_rps_source.R")
-source("~/GitHub/seattle-rps-analysis/Functions/Analytics/price_integral_source.R")
-source("~/GitHub/seattle-rps-analysis/Functions/Visualizations/price_comparison_source.R")
-source("~/GitHub/seattle-rps-analysis/Run/seattle_rps_compiler.R")
+# source("~/GitHub/seattle-rps-analysis/Functions/FinancialProcessing/inflation_stock_source.R")
+# source("~/GitHub/seattle-rps-analysis/Functions/Geocoding/zipcode_geocode_source.R")
+# source("~/GitHub/seattle-rps-analysis/Functions/RPSProcessing/seattle_rps_source.R")
+# source("~/GitHub/seattle-rps-analysis/Functions/Analytics/price_integral_source.R")
+# source("~/GitHub/seattle-rps-analysis/Functions/Visualizations/price_comparison_source.R")
+# source("~/GitHub/seattle-rps-analysis/Run/seattle_rps_compiler.R")
 
 # Load in data to start for full-run
 setwd(data_wd)
 
+
+# Set Spark connection
+sc <- spark_connect(master = connection_type)
+
 # Initial Processing
-rpsDf <- fread("EXTR_RPSale.csv", stringsAsFactors = FALSE)
-rbDf <- fread("EXTR_ResBldg.csv", stringsAsFactors = FALSE)
+rpsDf <- spark_read_csv(sc = sc, path = "EXTR_RPSale.csv", stringsAsFactors = FALSE)
+rbDf <- spark_read_csv(sc = sc, path = "EXTR_ResBldg.csv", stringsAsFactors = FALSE)
+
+rps_spark <- copy_to(sc, rpsDf)
+rb_spark <- copy_to(sc, rbDf)
 
 seattle_rps_compiler(rpsDf = rpsDf, rbDf = rbDf, 
                      latest_date = latest_date, topN = 10, 

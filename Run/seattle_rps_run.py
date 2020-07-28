6e0b901d172a28bd8f09b7574bec2a82f6ebb3d9
Daniel <danielhajialigol@gmail.com>
2020-07-28 18:10:57 -0400

    Updated runner

--- Run/seattle_rps_run.py
+++ Run/seattle_rps_run.py
@@ -1,21 +1,20 @@
 import pyspark
 import os
-import seattle_rps_compiler_test
+import seattle_rps_compiler
 from pyspark.sql import SparkSession
 
 # Enter latest date to perform analysis in format YYYY-MM-DD
 latest_date = "2019-12-31"
 
 # Initialize directories to save and extract data and get functions from
-save_wd = r""
-data_wd = r""
-function_wd = r""
+save_wd = r"C:\Users\15712\Documents\GitHub Projects\seattle-rps-analysis"
+data_wd = r"C:\Users\15712\Documents\GitHub Projects\seattle-rps-analysis\Data"
+function_wd = r"C:\Users\15712\Documents\GitHub Projects\seattle-rps-analysis\Functions"
 
 # Change to data directory
 os.chdir(data_wd)
 
 # Create Spark object
-# Note: this project is ran locally
 spark = SparkSession.builder.getOrCreate()
 
 # Read in csv files
@@ -27,5 +26,5 @@ zipcodes = spark.read.csv("Zipcode.csv", header = True)
 os.chdir(function_wd)
 
 # Call compiler function
-seattle_rps_compiler.seattle_rps_compiler(rps_spark = rps_spark, rb_spark = rb_spark, zipcodes = zipcodes, latest_date = latest_date,
-										  save_wd = save_wd, save_df = 'n', spark = spark)
+seattle_rps_compiler.seattle_rps_compiler(rps_spark = rps_spark, rb_spark = rb_spark, zipcodes = zipcodes,
+										  latest_date = latest_date, save_wd = save_wd, save_df = 'n', spark = spark)
